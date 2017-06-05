f105197f7dfbb1265e84e722feaf3a4fc3d2bd3c
chris <pachopa0320@gmail.com>
2017-06-05 23:51:31 +0000

    checked the contributor list in JSON format

---
+++ download_avatars.js
@@ -0,0 +1,20 @@
+var request = require('request');
+var GITHUB_USER = "pachopa";
+var GITHUB_TOKEN = "41ea54910b48572dad3235f7e678093e1436b466";
+console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+    var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+    console.log(requestURL)
+    // request(requestOptions, function(err, response, body) {
+    // if (err) { throw err; }
+    // cb(err, (JSON.parse(body)));
+//   });
+  
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
