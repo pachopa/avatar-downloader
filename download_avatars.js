fad08edffb54ce0a4ae5ea0f9ab15bed68c0142e
chris <pachopa0320@gmail.com>
2017-06-06 01:17:53 +0000

    go through each avatar image

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,8 @@
 var request = require('request');
+var fs = require('fs');
 var GITHUB_USER = "pachopa";
 var GITHUB_TOKEN = "41ea54910b48572dad3235f7e678093e1436b466";
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -13,11 +15,29 @@ function getRepoContributors(repoOwner, repoName, cb) {
     if (err) { throw err; }
     cb(err, (JSON.parse(body)));
   });
-  
+}
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+         .on('error', function (err) {
+             throw err;
+         })
+         .on('response', function (response) {
+             console.log('Downloading image...')
+         })
+         .on('end', function() {
+             console.log('download complete.')
+         })
+         .pipe(fs.createWriteStream('./avatars/' + filePath + ".jpg"))
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  //console.log("Errors:", err);
+  //console.log("Result:", result);
+  result.forEach(function(image) {
+    var avatar = image.avatar_url;
+    var users = image.login;
+    downloadImageByURL(avatar, users)
+  });
 });
 
