edb943ca9d66a10faad0df35cc5ef76a6b97b7b9
chris <pachopa0320@gmail.com>
2017-06-06 00:04:37 +0000

    Printed Each avatar URL

--- download_avatars.js
+++ download_avatars.js
@@ -5,11 +5,14 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
     var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-    console.log(requestURL)
-    // request(requestOptions, function(err, response, body) {
-    // if (err) { throw err; }
-    // cb(err, (JSON.parse(body)));
-//   });
+    var requestOptions = {
+      url: requestURL,
+      headers: {"User-Agent": "pachopa"}
+    };
+    request(requestOptions, function(err, response, body) {
+    if (err) { throw err; }
+    cb(err, (JSON.parse(body)));
+  });
   
 }
 
